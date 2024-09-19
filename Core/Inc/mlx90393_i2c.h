cf9ab4a92ea373119ff0321afdb36b2abc2ae426
Henry Wu <h27wu@uwaterloo.ca>
2024-09-18 23:02:18 -0400

    Basic functions not tested

--- Core/Inc/mlx90393_i2c.h
+++ Core/Inc/mlx90393_i2c.h
@@ -8,27 +8,41 @@
 #ifndef INC_MLX90393_I2C_H_
 #define INC_MLX90393_I2C_H_
 
-#define DEFAULT_I2C_ADDRESS 0x0F
+#define DEFAULT_I2C_ADDRESS 0x0F << 1 //Set I2C address
+#define CS GPIO_PIN_5 //Set CS pin
+// Flags to use with "zyxt" variables.
+#define MLX90393_T  0x01  /* Temperature */
+#define MLX90393_X  0x02  /* X-axis */
+#define MLX90393_Y  0x04  /* Y-axis */
+#define MLX90393_Z  0x08  /* Z-axis */
 
-class MLX90393
-{
+
+class MLX90393{
 	public:
 		MLX90393();
-		bool begin_I2C(uint8_t i2c_addr = DEFAULT_I2C_ADDRESS);
+		void mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt); //Single measurement
+		void mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt); //Read measurement
+		int mlx90393_zyxt_set_bits(uint8_t zyxt);
+		uint16_t mlx90393_decode_helper(uint8_t *data);
+
 
 	private:
+		uint16_t mlx90393_t;
+		uint16_t mlx90393_x;
+		uint16_t mlx90393_y;
+		uint16_t mlx90393_z;
+
+		void mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_t tx_size, uint16_t rx_size);
+		void mlx90393_decode(uint8_t *rx_data, uint8_t zyxt);
 
 };
 
-void mlx90393_i2c_init(void);
 
-void mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_t tx_size, uint16_t rx_size);
 
-void mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt);
 
-void mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt);
 
-int zyxt_set_bits(uint8_t zyxt);
+
+
 
 
 #endif /* INC_MLX90393_I2C_H_ */
--- Core/Src/mlx90393_i2c.cpp
+++ Core/Src/mlx90393_i2c.cpp
@@ -5,33 +5,35 @@
  *      Author: Henry
  */
 #include "mlx90393_i2c.h"
-#define I2C_ADDRESS 0x0F << 1 //Determined by A0 A1 connection
-#define CS GPIO_PIN_5
 
-void mlx90393_i2c_init(void)
-{
+MLX90393::MLX90393(){
 	HAL_GPIO_WritePin(GPIOB, CS, GPIO_PIN_SET);
+	uint16_t mlx90393_t = 0;
+	uint16_t mlx90393_x = 0;
+	uint16_t mlx90393_y = 0;
+	uint16_t mlx90393_z = 0;
 }
 
-void mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_t tx_data, uint16_t rx_data)
+void MLX90393::mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_t tx_data, uint16_t rx_data)
 {
-	HAL_I2C_Master_Transmit(&hi2c3, I2C_ADDRESS, tx_data, tx_size, HAL_MAX_DELAY);
-	HAL_I2C_Master_Receive(&hi2c3, I2C_ADDRESS + 1, rx_data, rx_size, HAL_MAX_DELAY);
+	HAL_I2C_Master_Transmit(&hi2c3, DEFAULT_I2C_ADDRESS, tx_data, tx_size, HAL_MAX_DELAY);
+	HAL_I2C_Master_Receive(&hi2c3, DEFAULT_I2C_ADDRESS + 1, rx_data, rx_size, HAL_MAX_DELAY);
 }
 
-void mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt)
+void MLX90393::mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt)
 {
 	uint8_t tx_data = (0x30)| zyxt;
 	mlx90393_i2c_transmit(tx_data, rx_data, 1, 1);
 }
 
-void mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt){
-	int rx_size = zyxt_set_bits(zyxt);
+void MLX90393::mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt){
+	int rx_size = zyxt_set_bits(zyxt) + 1;
 	uint8_t tx_data = (0x40) | zyxt;
 	mlx90393_i2c_transmit(tx_data, rx_data, 1, 2 * rx_size + 1);
+	mlx90393_decode(rx_data, zyxt);
 }
 
-int zyxt_set_bits(uint8_t zyxt){
+int MLX90393::mlx90393_zyxt_set_bits(uint8_t zyxt){
 	int count = 0;
 	while(zyxt){
 		if(zyxt & 0x1){
@@ -40,4 +42,29 @@ int zyxt_set_bits(uint8_t zyxt){
 		zyxt >> 1;
 	}
 	return count;
+}
+
+void MLX90393::mlx90393_decode(uint8_t *rx_data, u8int_t zyxt){
+	uint8_t *cursor = rx_data;
+	cursor += 1; //Skip status byte
+	if(zyxt & MLX90393_T){
+		mlx90393_t = mlx90393_decode_helper(cursor);
+		cursor += 2;
+	}
+	if(zyxt & MLX90393_X){
+		mlx90393_x = mlx90393_decode_helper(cursor);
+		cursor += 2;
+	}
+	if(zyxt & MLX90393_Y){
+		mlx90393_y = mlx90393_decode_helper(cursor);
+		cursor += 2;
+	}
+	if(zyxt & MLX90393_Z){
+		mlx90393_z = mlx90393_decode_helper(cursor);
+		cursor += 2;
+	}
+}
+
+uint16_t MLX90393::mlx90393_decode_helper(uint8_t *data){
+	return (data[0] << 8 + data[1]);
 }
