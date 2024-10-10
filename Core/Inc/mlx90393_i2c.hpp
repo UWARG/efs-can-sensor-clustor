86e687de3ea11f57a0a557a8fc43219affb96d72
Henry Wu <h27wu@uwaterloo.ca>
2024-10-10 00:20:50 -0400

    Organized header file, implemented raw data conversion

--- Core/Inc/mlx90393_i2c.hpp
+++ Core/Inc/mlx90393_i2c.hpp
@@ -11,34 +11,59 @@
 #define DEFAULT_I2C_ADDRESS 0x0C << 1 //Set I2C address
 #define CS GPIO_PIN_5 //Set CS pin
 // Flags to use with "zyxt" variables.
-#define MLX90393_T  0x01  /* Temperature */
-#define MLX90393_X  0x02  /* X-axis */
-#define MLX90393_Y  0x04  /* Y-axis */
-#define MLX90393_Z  0x08  /* Z-axis */
-#define MLX90393_CONF1 0x00         /**< Gain */
-#define MLX90393_CONF2 0x01         /**< Burst, comm mode */
-#define MLX90393_CONF3 0x02         /**< Oversampling, filter, res. */
-#define MLX90393_CONF4 0x03         /**< Sensitivty drift. */
-#define MLX90393_GAIN_SHIFT 4       /**< Left-shift for gain bits. */
+#define MLX90393_T  0x01  // Temperature
+#define MLX90393_X  0x02  // X-axis
+#define MLX90393_Y  0x04  // Y-axis
+#define MLX90393_Z  0x08  // Z-axis
+
+#define MLX90393_CONF1 0x00         // Gain
+#define MLX90393_CONF2 0x01         // Burst, comm mode
+#define MLX90393_CONF3 0x02         // Oversampling, filter, res
+#define MLX90393_CONF4 0x03         // Sensitivty drift
+//Shifts and masks
+#define MLX90393_GAIN_SHIFT 4       // Left-shift for gain bits
 #define MLX90393_GAIN_MASK 0x0070	// Mask to clear bits 4-6
 #define MLX90393_X_RES_MASK 0x00C0  // Mask to clear bits 5-6
 #define MLX90393_Y_RES_MASK 0x0180  // Mask to clear bits 7-8
 #define MLX90393_Z_RES_MASK 0x0600  // Mask to clear bits 9-10
-#define MLX90393_X_RES_SHIFT 5       /**< Left-shift for x_res bits. */
-#define MLX90393_Y_RES_SHIFT 7       /**< Left-shift for y_res bits. */
-#define MLX90393_Z_RES_SHIFT 9       /**< Left-shift for z_res bits. */
-#define MLX90393_HALL_CONF 0x0C     /**< Hall plate spinning rate adj. */
+#define MLX90393_X_RES_SHIFT 5       // Left-shift for x_res bits
+#define MLX90393_Y_RES_SHIFT 7       // Left-shift for y_res bits
+#define MLX90393_Z_RES_SHIFT 9       // Left-shift for z_res bits
+#define MLX90393_HALL_CONF 0x0C     // Hall plate spinning rate adj
+#define MLX90393_RES_17 0x8000		// Mask to remove sign bit for res = 2 (MSB = bit 17)
+#define MLX90393_RES_18 0x4000		// Mask to remove sign bit for res = 3 (MSB = bit 18)
+//Commands
+#define CMD_NOP 0x00
+#define	CMD_EXIT 0x80
+#define	CMD_START_BURST 0x10
+#define	CMD_WAKE_ON_CHANGE 0x20
+#define	CMD_START_MEASUREMENT 0x30
+#define	CMD_READ_MEASUREMENT 0x40
+#define	CMD_READ_REGISTER 0x50
+#define	CMD_WRITE_REGISTER 0x60
+#define	CMD_MEMORY_RECALL 0xD0
+#define	CMD_MEMORY_STORE 0xE0
+#define	CMD_RESET 0xF0
+//Status bits
+#define BURST_MODE_BIT 0x80
+#define WAKE_ON_CHANGE_BIT 0x40
+#define POLLING_MODE_BIT 0x20
+#define ERROR_BIT 0x10
+#define EEC_BIT 0x08,
+#define RESET_BIT 0x04
+#define D1_BIT 0x02
+#define D0_BIT 0x01
 
 class MLX90393{
 	public:
 		MLX90393();
-		bool mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt); //Start single measurement
-		bool mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt); //Read measurement
-		bool mlx90393_i2c_EX();
-		bool mlx90393_i2c_RT();
-		bool mlx90393_i2c_WR(uint8_t reg, uint8_t *tx_data);
-		bool mlx90393_i2c_RR(uint8_t reg);
-		bool mlx90393_has_error();
+		bool mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt); //Start single measurement mode cmd
+		bool mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt); //Read measurement cmd
+		bool mlx90393_i2c_EX(); //Exit mode cmd
+		bool mlx90393_i2c_RT(); //Reset cmd
+		bool mlx90393_i2c_WR(uint8_t reg, uint8_t *tx_data); //Write register cmd
+		bool mlx90393_i2c_RR(uint8_t reg); // Read register cmd
+		bool mlx90393_has_error(); //Check status bit error
 		int mlx90393_zyxt_set_bits(uint8_t zyxt);
 		bool mlx90393_set_gain(uint8_t gain);
 		bool mlx90393_get_gain();
@@ -50,38 +75,54 @@ class MLX90393{
 		uint16_t mlx90393_decode_helper(uint8_t *data);
 		void mlx90393_convert_raw();
 
-		enum { BURST_MODE_BIT = 0x80, WAKE_ON_CHANGE_BIT = 0x40,
-		       POLLING_MODE_BIT = 0x20, ERROR_BIT = 0x10, EEC_BIT = 0x08,
-		       RESET_BIT = 0x04, D1_BIT = 0x02, D0_BIT = 0x01 };
-
-		enum {
-		  CMD_NOP = 0x00,
-		  CMD_EXIT = 0x80,
-		  CMD_START_BURST = 0x10,
-		  CMD_WAKE_ON_CHANGE = 0x20,
-		  CMD_START_MEASUREMENT = 0x30,
-		  CMD_READ_MEASUREMENT = 0x40,
-		  CMD_READ_REGISTER = 0x50,
-		  CMD_WRITE_REGISTER = 0x60,
-		  CMD_MEMORY_RECALL = 0xd0,
-		  CMD_MEMORY_STORE = 0xe0,
-		  CMD_RESET = 0xf0
-		};
-
 	private:
-		uint16_t mlx90393_t;
-		uint16_t mlx90393_x;
-		uint16_t mlx90393_y;
-		uint16_t mlx90393_z;
-		uint8_t mlx90393_status;
-		uint16_t mlx90393_reg_val;
-		uint8_t mlx90393_gain;
-		uint8_t mlx90393_x_res;
-		uint8_t mlx90393_y_res;
-		uint8_t mlx90393_z_res;
+		typedef struct raw{
+		    uint16_t t;
+		    uint16_t x;
+		    uint16_t y;
+		    uint16_t z;
+		} raw;
+
+		typedef struct converted{
+		    float t;
+		    float x;
+		    float y;
+		    float z;
+		} converted;
+
+		typedef struct reg{
+			uint8_t stat;
+			uint16_t val;
+			uint8_t gain;
+			uint8_t x_res;
+			uint8_t y_res;
+			uint8_t z_res;
+			uint8_t hallconf;
+			uint8_t tcmp_en;
+		}reg;
+
+		const float sens_lookup_0xC[8][4][2] = {
+	        /* GAIN_SEL = 0, 5x gain */
+	        {{0.751, 1.210}, {1.502, 2.420}, {3.004, 4.840}, {6.009, 9.680}},
+	        /* GAIN_SEL = 1, 4x gain */
+	        {{0.601, 0.968}, {1.202, 1.936}, {2.403, 3.872}, {4.840, 7.744}},
+	        /* GAIN_SEL = 2, 3x gain */
+	        {{0.451, 0.726}, {0.901, 1.452}, {1.803, 2.904}, {3.605, 5.808}},
+	        /* GAIN_SEL = 3, 2.5x gain */
+	        {{0.376, 0.605}, {0.751, 1.210}, {1.502, 2.420}, {3.004, 4.840}},
+	        /* GAIN_SEL = 4, 2x gain */
+	        {{0.300, 0.484}, {0.601, 0.968}, {1.202, 1.936}, {2.403, 3.872}},
+	        /* GAIN_SEL = 5, 1.667x gain */
+	        {{0.250, 0.403}, {0.501, 0.807}, {1.001, 1.613}, {2.003, 3.227}},
+	        /* GAIN_SEL = 6, 1.333x gain */
+	        {{0.200, 0.323}, {0.401, 0.645}, {0.801, 1.291}, {1.602, 2.581}},
+	        /* GAIN_SEL = 7, 1x gain */
+	        {{0.150, 0.242}, {0.300, 0.484}, {0.601, 0.968}, {1.202, 1.936}},
+		};
 
 		void mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_t tx_size, uint16_t rx_size);
 		void mlx90393_decode(uint8_t *rx_data, uint8_t zyxt);
+		void mlx90393_convert();
 
 };
 
--- Core/Src/mlx90393_i2c.cpp
+++ Core/Src/mlx90393_i2c.cpp
@@ -8,10 +8,6 @@
 
 MLX90393::MLX90393(){
 	HAL_GPIO_WritePin(GPIOB, CS, GPIO_PIN_SET);
-	uint16_t mlx90393_t = 0;
-	uint16_t mlx90393_x = 0;
-	uint16_t mlx90393_y = 0;
-	uint16_t mlx90393_z = 0;
 }
 
 void MLX90393::mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_t tx_size, uint16_t rx_size)
@@ -19,7 +15,7 @@ void MLX90393::mlx90393_i2c_transmit(uint8_t *tx_data, uint8_t *rx_data, uint16_
 	uint8_t rx_buf[rx_size + 2];
 	HAL_I2C_Master_Transmit(&hi2c3, DEFAULT_I2C_ADDRESS, tx_data, tx_size, HAL_MAX_DELAY);
 	HAL_I2C_Master_Receive(&hi2c3, DEFAULT_I2C_ADDRESS + 1, rx_buf, rx_size + 1, HAL_MAX_DELAY);
-	mlx90393_status = rx_buf[0];
+	reg.stat = rx_buf[0];
 	for(int i = 0; i < rx_size; i++){
 		rx_data[i] = rx_buf[i + 1];
 	}
@@ -29,7 +25,7 @@ bool MLX90393::mlx90393_i2c_SM(uint8_t *rx_data, uint8_t zyxt)
 {
 	uint8_t tx_data = CMD_START_MEASUREMENT | zyxt;
 	mlx90393_i2c_transmit(tx_data, rx_data, 1, 1);
-	return !(mlx_90393_has_error() || (mlx90393_status & POLLING_MODE_BIT) == 0);
+	return !(mlx_90393_has_error() || (reg.stat & POLLING_MODE_BIT) == 0);
 }
 
 bool MLX90393::mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt){
@@ -37,6 +33,7 @@ bool MLX90393::mlx90393_i2c_RM(uint8_t *rx_data, uint8_t zyxt){
 	uint8_t tx_data = CMD_READ_MEASUREMENT | zyxt;
 	mlx90393_i2c_transmit(tx_data, rx_data, 1, 2 * rx_size + 1);
 	mlx90393_decode(rx_data, zyxt);
+	mlx90393_convert();
 	return !mlx_90393_has_error();
 }
 
@@ -47,7 +44,7 @@ bool MLX90393::mlx90393_i2c_EX(){
 
 bool MLX90393::mlx90393_i2c_RT(){
 	mlx90393_i2c_transmit(CMD_RESET, nullptr, 1, 0);
-	return !(mlx90393_has_error() || (mlx90393_status & RESET_BIT) == 0);
+	return !(mlx90393_has_error() || (mlx90393_stat & RESET_BIT) == 0);
 }
 
 bool MLX90393::mlx90393_i2c_WR(uint8_t reg, uint8_t *tx_data){
@@ -57,34 +54,38 @@ bool MLX90393::mlx90393_i2c_WR(uint8_t reg, uint8_t *tx_data){
 	return true;
 }
 bool MLX90393::mlx90393_i2c_RR(uint8_t reg){
-	if(HAL_I2C_Mem_Write(&hi2c3, DEFAULT_I2C_ADDRESS, reg << 2, I2C_MEMADD_SIZE_16BIT, &mlx90393_reg_val, 1, HAL_MAX_DELAY) != HAL_OK){
+	if(HAL_I2C_Mem_Write(&hi2c3, DEFAULT_I2C_ADDRESS, reg << 2, I2C_MEMADD_SIZE_16BIT, &reg.val, 1, HAL_MAX_DELAY) != HAL_OK){
 		return false;
 	}
 	return true;
 }
 
 bool mlx90393_set_gain(uint8_t gain){
-	if(mlx90393_i2c_RR(MLX90393_CONF1)){
+	if(!mlx90393_i2c_RR(MLX90393_CONF1)){
 		return false;
 	}
-	uint16_t data = mlx90393_reg_val & MLX90393_GAIN_MASK;
+	uint16_t data = reg.val & MLX90393_GAIN_MASK;
 	data |= gain << MLX90393_GAIN_SHIFT;
+	reg.gain = gain;
 	return(mlx90393_i2c_WR(MLX90393_CONF1, &data));
 }
 
 bool mlx90393_get_gain(){
-	if(mlx90393_i2c_RR(MLX90393_CONF1)){
+	if(!mlx90393_i2c_RR(MLX90393_CONF1)){
 		return false;
 	}
-	uint16_t data = mlx90393_reg_val & MLX90393_GAIN_MASK;
-	mlx90393_gain = data >> MLX90393_GAIN_SHIFT;
+	uint16_t data = reg.val & MLX90393_GAIN_MASK;
+	reg.gain = data >> MLX90393_GAIN_SHIFT;
 	return true;
 }
 
 bool mlx90393_set_resolution(uint8_t x_res, uint8_t y_res, uint8_t z_res){
-	if(mlx90393_i2c_RR(MLX90393_CONF3)){
+	if(!mlx90393_i2c_RR(MLX90393_CONF3)){
 		return false;
 	}
+	reg.x_res = x_res;
+	reg.y_res = y_res;
+	reg.z_res = z_res;
 	uint16_t data = mlx90393_reg_val;
 	if(x_res != 0){
 		data = (data & MLX90393_X_RES_MASK) | (x_res << MLX90393_X_RES_SHIFT);
@@ -103,15 +104,15 @@ bool mlx90393_get_resolution(){
 	if(mlx90393_i2c_RR(MLX90393_CONF3)){
 		return false;
 	}
-	uint16_t data = mlx90393_reg_val;
-	mlx90393_x_res = (data & MLX90393_X_RES_MASK) >> MLX90393_X_RES_SHIFT;
-	mlx90393_y_res = (data & MLX90393_Y_RES_MASK) >> MLX90393_Y_RES_SHIFT;
-	mlx90393_z_res = (data & MLX90393_Z_RES_MASK) >> MLX90393_Z_RES_SHIFT;
+	uint16_t data = reg.val;
+	reg.x_res = (data & MLX90393_X_RES_MASK) >> MLX90393_X_RES_SHIFT;
+	reg.y_res = (data & MLX90393_Y_RES_MASK) >> MLX90393_Y_RES_SHIFT;
+	reg.z_res = (data & MLX90393_Z_RES_MASK) >> MLX90393_Z_RES_SHIFT;
 	return true;
 }
 
 bool MLX90393::mlx90393_has_error(){
-	return (mlx90393_status & ERROR_BIT) != 0;
+	return (reg.stat & ERROR_BIT) != 0;
 }
 
 int MLX90393::mlx90393_zyxt_set_bits(uint8_t zyxt){
@@ -127,28 +128,55 @@ int MLX90393::mlx90393_zyxt_set_bits(uint8_t zyxt){
 
 void MLX90393::mlx90393_decode(uint8_t *rx_data, u8int_t zyxt){
 	uint8_t *cursor = rx_data;
-	mlx90393_status = rx_data[0];
+	reg.stat = rx_data[0];
 	cursor += 1; //Skip status byte
 	if(zyxt & MLX90393_T){
-		mlx90393_t = mlx90393_decode_helper(cursor);
+		raw.t = mlx90393_decode_helper(cursor);
 		cursor += 2;
 	}
 	if(zyxt & MLX90393_X){
-		mlx90393_x = mlx90393_decode_helper(cursor);
+		raw.x = mlx90393_decode_helper(cursor);
 		cursor += 2;
 	}
 	if(zyxt & MLX90393_Y){
-		mlx90393_y = mlx90393_decode_helper(cursor);
+		raw.y = mlx90393_decode_helper(cursor);
 		cursor += 2;
 	}
 	if(zyxt & MLX90393_Z){
-		mlx90393_z = mlx90393_decode_helper(cursor);
+		raw.z = mlx90393_decode_helper(cursor);
 		cursor += 2;
 	}
 }
 
 uint16_t MLX90393::mlx90393_decode_helper(uint8_t *data){
 	return (data[0] << 8 | data[1]);
+}
+
+void MLX90393::mlx90393_convert(){
+	//Only when tcmp_en = 0
+	//Remove sign bit for unsigned res settings
+	if(reg.x_res == 2){
+		raw.x -= MLX90393_RES_17;
+	}
+	if(reg.x_res == 3){
+		raw.x -= MLX90393_RES_18;
+	}
+	if(reg.y_res == 2){
+		raw.y -= MLX90393_RES_17;
+	}
+	if(reg.y_res == 3){
+		raw.y -= MLX90393_RES_18;
+	}
+	if(reg.z_res == 2){
+		raw.z -= MLX90393_RES_17;
+	}
+	if(reg.x_res == 2){
+		raw.z -= MLX90393_RES_18;
+	}
+	//Convert raw data base on sensitivity
+	converted.x = (float)raw.x * sens_lookup_0xC[reg.gain][reg.rex_x][0];
+	converted.y = (float)raw.y * sens_lookup_0xC[reg.gain][reg.rex_y][0];
+	converted.z = (float)raw.z * sens_lookup_0xC[reg.gain][reg.rex_z][1];
 }
 
 void MLX90393::mlx90393_convert_raw(){
